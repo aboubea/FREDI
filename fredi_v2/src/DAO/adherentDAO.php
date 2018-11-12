6e8ebbf3a4d525c453d6244eeeee7426656362ad
pacobarbe31 <paco.barbe@limayrac.fr>
2018-11-12 17:26:10 +0100

    AdherentDAO en cours

---
+++ fredi_v2/src/DAO/adherentDAO.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Classe d'accès AdherentDAO
+ *
+ * @author Paco
+ */
+require_once SRC . DS . 'models' . DS . 'Adherent.php';
+require_once SRC . DS . 'framework' . DS . 'DAO.php';
+
+class AdherentDAO extends DAO {
+
+  /**
+   * Lecture d'un adherent par son ID
+   *
+   * @param type $id_adherent
+   * @return \Adherent
+   */
+  function find($id_adherent) {
+    $sql = "select * from adherent where id_adh=:id_adh";
+    $params = array(":id_adh" => $id_adh);
+    $sth = $this->executer($sql, $params);
+    $row = $sth->fetch(PDO::FETCH_ASSOC);
+    //TODO : si le fetch ne retourne rien, $row=false et ça plante l'hydrateur
+    $adherent = new Adherent($row);
+    // Retourne l'objet métier
+    return $adherent;
+  }
+
+  /**
+   * Lecture de tous les adherents
+   * @return \Adherent
+   */
+  function findAll() {
+    $sql = "select * from adherent";
+    $sth = $this->executer($sql);
+    $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
+    $tableau = array();
+    foreach ($rows as $row) {
+      $tableau[] = new Adherent($row);
+    }
+    // Retourne un tableau d'objets métier
+    return $tableau;
+  }
+
+  /**
+   * Retourne tous les adherents identifié par un login
+   * @return array \Adherent
+   */
+  function findAllByLogin($login) {
+    $sql = "select * from adherent where mail_inscrit=:mail_inscrit";
+    $params = array(":mail_inscrit" => $mail_inscrit);
+    $sth = $this->executer($sql, $params);
+    $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
+    $tableau = array();
+    foreach ($rows as $row) {
+      $tableau[] = new Adherent($row);
+    }
+    // Retourne un tableau d'objets métier
+    return $tableau;
+  }
+
+  /**
+   * Ajoute un adherents
+   * @param Adherent $adherent
+   * @return type
+   */
+  function insert(Adherent $adherent) {
+    $sql = "insert into adherent (mail_inscrit, mdp_inscrit) values (:login, :mdp_inscrit)";
+    $params = array(
+        ":mail_inscrit" => $utilisateur->get_mail_inscrit(),
+        ":mdp_inscrit" => $utilisateur->get_mdp_inscrit()
+    );
+    $sth = $this->executer($sql, $params);
+    $nb = $sth->rowcount();
+    return $nb;  // Retourne le nombre de mise à jour
+  }
+
+}
+
+// Classe AdherentDAO
