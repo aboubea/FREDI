24784a6df7c15ffd132aaf70b75fc46026111ee7
aboubea <antoine.boubeastugue@outlook.fr>
2018-11-24 15:32:47 +0100

    login.php OK register.php(espace inscription) OK

--- fredi_v2/login.php
+++ fredi_v2/login.php
@@ -1,22 +1,25 @@
+  <?php
+  //
+  // PPE Projet FREDI
+  //
+  include 'src/config/init.php';
+  ?>
 
-<div id=1>
-    
-<center><h1>Se connecter</h1></center>
-                <br />
-                <br />
-                <div>
-                    <form method="POST" action="testlog.php">
-                    <center>
-                        <input id="in" type="text" name="pseudo" placeholder="Identifiant" x-webkit-speech></br></br>
-                        <input id="in" type="password" name="mdp" placeholder="Mot de Passe"  x-webkit-speech></br></br>
-                        <input id="envoy" type="submit" value="Envoyer">
-                    </center>
-                    </form>
+  <!DOCTYPE html>
+  <html>
+  <head>
+  <meta charset="UTF-8">
+  <title><?php echo APPLINAME ; ?></title>
+  <link rel="stylesheet" type="text/css" href="src/css/styles.css" />
+  </head>
+  <body>
+  <h1><?php echo APPLINAME ; ?></h1>
 
-                </div>
-
-</div>
-            
-  
-
-   
+  <h2>Se connecter</h2>
+  <br />
+  <br />
+  <form method="POST" action="testlog.php">
+    <input id="in" type="text" name="pseudo" placeholder="Identifiant"></br></br>
+    <input id="in" type="password" name="mdp" placeholder="Mot de Passe"></br></br>
+    <input id="envoy" type="submit" value="Connexion">
+  </form>
--- fredi_v2/register.php
+++ fredi_v2/register.php
@@ -1,37 +1,32 @@
-<center>
-<h1>Inscription</h1>
-            
-    
-        <form action="testp01.php" method="POST">
+<?php
+//
+// PPE Projet FREDI
+//
+include 'src/config/init.php';
+?>
 
-            </br>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title><?php echo APPLINAME ; ?></title>
+<link rel="stylesheet" type="text/css" href="src/css/styles.css" />
+</head>
+<body>
+<h1><?php echo APPLINAME ; ?></h1>
+
+<h2>S'Inscrire</h2>
+<form action="testp01.php" method="POST">
             Entrez votre pseudo: <input id="in" type="text" name="PS">
             </br>
             Entrez votre mot de passe: <input id="in" type="text" name="MDP">
             </br>
-			Comfirmez votre mot de passe: <input id="in" type="text" name="MDP1">
-			</br>
+            Comfirmez votre mot de passe: <input id="in" type="text" name="MDP1">
+			      </br>
             Entrez votre mail: <input id="in" type="text" name="MA">
             </br>
-            Selectionnez votre ligue: <select id="in" name="ligue" size="1"></br>
-                <option value="1">Football</option>
-                <option value="2">Pétanque</option>
-                <option value="3">Judo</option>
-
-            </select>
-
-            <select style="display:none;" id="in" name="TU" size="1"></br>
-                <option value="1">normal</option>
-
-            </select>
-            </br>
-
-
-            <input id="envoy" type="submit" value="OK"/>
+            <input id="envoy" type="submit" value="Inscription"/>
 
         </form>
-
-
-    </center>
 
     </div>
