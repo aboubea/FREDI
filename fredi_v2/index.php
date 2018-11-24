a70203ae18ebd519ff9bfeb184b9c9052874be16
aboubea <antoine.boubeastugue@outlook.fr>
2018-11-24 15:16:16 +0100

    page connexion et inscription   -> index OK   -> page 1 OK

--- fredi_v2/index.php
+++ fredi_v2/index.php
@@ -1,7 +1,29 @@
 <?php
-include 'head.php';
-include 'base.php';
-include 'logo.php';
-include 'team.php';
-include 'footer.php';
+//
+// PPE Projet FREDI
+//
+include 'src/config/init.php';
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title><?php echo APPLINAME ; ?></title>
+<link rel="stylesheet" type="text/css" href="src/css/styles.css" />
+</head>
+<body>
+<h1><?php echo APPLINAME ; ?></h1>
+
+<h2>Bienvenue sur notre site <?php echo APPLINAME?> !</h2>
+
+<p>Bienvenue sur ce site qui correspond au projet du groupe 1 SIO 18/19</p>
+<br>
+<p>Vous êtes adhérents, cliquez <a href="page1.php">ici</a></p>
+<p>Vous êtes trésorier, cliquez <a href="page2.php">ici</a></p>
+<p>Vous êtes membre du CRIB, cliquez <a href="page3.php">ici</a></p>
+
+
+
+</body>
+</html>
