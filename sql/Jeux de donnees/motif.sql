85bc0f635958bcb9d7755a8abe967d40a57c89f7
Paco Barbé <paco.barbe@limayrac.fr>
2018-12-10 02:13:19 +0100

    Ajout des jeux de données + fichiers CSV

---
+++ sql/Jeux de donnees/motif.sql
@@ -0,0 +1,5 @@
+INSERT INTO motif(id_motif,libelle_motif) VALUES (1,'Reunion');
+INSERT INTO motif(id_motif,libelle_motif) VALUES (2,'Competition regionale');
+INSERT INTO motif(id_motif,libelle_motif) VALUES (3,'Competition nationale');
+INSERT INTO motif(id_motif,libelle_motif) VALUES (4,'Competition internationnale');
+INSERT INTO motif(id_motif,libelle_motif) VALUES (5,'Stage');
