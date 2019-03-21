2d9e5bcfa90afa0f0ef9943afc42181972409f7a
pacobarbe31 <paco.barbe@limayrac.fr>
2019-03-21 15:42:56 +0100

    [UPDATE] Correction titres/sous Titres

--- edit_tarif_km.php
+++ edit_tarif_km.php
@@ -16,32 +16,34 @@ $tresorier= $tresorierDAO->findByMail($mail_tresorier);
 
 $indemniteDAO = new IndemniteDAO;
 
+$annee = isset($_GET['annee']) ? $_GET['annee'] : $_POST['annee'] ;
+$indemnite = $indemniteDAO->findIndemnite($annee);
+
 ?>
 <html>
 <body>
 
 <?php include 'menu3.php' ; ?>
         
-        <!-- Hero-Section
-          ================================================== -->
+        <!-- Hero-Section ================================================== -->
 
         <div class="hero row">
             <div class="hero-right col-sm-6 col-sm-6">
-                <h1 class="header-headline bold">Ajout du tarif </h1>
-                <h4 class="header-running-text light"> Trésorier : <?php echo $tresorier->getprenom_tresorier() . ' ' . $tresorier->getnom_tresorier() ;?> ></h4>
+                <h1 class="header-headline bold">Edition du tarif </h1>
+                <h4 class="header-running-text light"> Trésorier : <?php echo $tresorier->getprenom_tresorier() . ' ' . $tresorier->getnom_tresorier() ; ?> ></h4>
                 </div><!--hero-left-->
                 <div class="base">
 
 <!-- DEBUT BASE --------------------------------------------------------------------------------------------------------------- -->
 
-<h2 align='center'>Insertion du tarif kilométrique</h2>
+<h2 align='center'>Edition du tarif kilométrique</h2>
 
 <?php
 
 $submit = isset($_POST['submit']);
 
 if($submit){
-    $annee = date('Y');
+    $annee = isset($_GET['annee']) ? $_GET['annee'] : $_POST['annee'] ;
     $tarif_kilometrique = isset($_POST['tarif_kilometrique']) ? $_POST['tarif_kilometrique'] : "";
 
     $indemnite =  new Indemnite(array(
@@ -49,18 +51,17 @@ if($submit){
       ":tarif_kilometrique" => $tarif_kilometrique,
     ));
 
-       $nb = $indemniteDAO->insert($annee, $tarif_kilometrique);
+    // Met à jour à l'enregistrement dans la BDD
+    $nb = $indemniteDAO->updateTarif($indemnite);
 
         header('Location: see_tarif_km.php?success=success');
-        
+
         // Obligatoire sinon PHP continue à exécuter le script
         exit;  
-} else {
-    $erreur = "<p align='center'><strong>Vous n'avez pas saisis toutes les informations ! Veuillez remplir tous les champs svp.</strong></p>";
-}
+} 
 
 // Ajout du formulaire d'inscription
-    include 'forms/Tresorier_insertion_tarif_km.php';
+    include 'forms/Tresorier_modifier_tarif_km.php';
 ?>
 
 <p align="center"><a href="<?php echo $_SERVER['HTTP_REFERER']; ?>">Retour</a> | <a href="index.php">Page d'accueil</a></p>
