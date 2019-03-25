5cde1b1a356562c6a072da2e9220544de8ae80fe
pacobarbe31 <paco.barbe@limayrac.fr>
2019-03-25 13:37:03 +0100

    [UPDATE] Correction affichage bordereau majeur

--- list_borderaux.php
+++ list_borderaux.php
@@ -2,19 +2,19 @@
 include 'head.php';
 include 'init.php';
 session_start();
-if(isset($_SESSION['mail_inscrit'])){
+
+if (isset($_SESSION['mail_inscrit'])) {
+    // Si l'adhérent MAJEUR est connecté, on récupère son mail stocké en SESSION lors de la connexion
     $mail_inscrit = $_SESSION['mail_inscrit'];
-}elseif(isset($_GET['mail_inscrit']) && isset($_SESSION['mail_resp_leg'])){
-    $mail_inscrit = $_GET['mail_inscrit'];
+
+    // Et les infos de l'adherent majeur dans $adherent (tableau objet) et notamment sa licence
+    $adherentDAO = new AdherentDAO();
+    $adherent= $adherentDAO->findByMail($mail_inscrit);
+    $licence_adh = $adherent->getLicence_adh();
 }else{
+    // Sinon on redirige vers la page d'accueil avec un message d'erreur
     header('Location:index.php?private=1');
-  }
-
-$adherentDAO = new AdherentDAO();
-$adherent= $adherentDAO->findByMail($mail_inscrit);
-
-
-$licence_adh = $adherent->getLicence_adh();
+}
 
 $notefraisDAO = new NotefraisDAO();
 $notes= $notefraisDAO->findbylicence($licence_adh);
@@ -85,7 +85,6 @@ if (count($notes) !== 0){
     echo 'Voulez vous créer votre premier borderau ?';
     echo'</br>';echo'</br>';
     echo '<p><a class="btn btn-primary" href="create_bordereau.php" role="button">Créer un bordereau »</a></p>';
-
 }
 
 ?>
